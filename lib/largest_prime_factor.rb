8fdeaaa54a0ea5b3c7e1b7a5eaf37111aca56ef0
elainapolson <elainapolson@gmail.com>
2015-07-12 11:52:14 -0400

    soooo slowww

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,23 @@
+require 'pry'
 # Enter your procedural solution here!
+
+#find all the factors
+#find the prime factors
+#find the largest
+
+
+def is_prime?(f) 
+  Math.sqrt(f).to_i.downto(2).each do |i| 
+    return false if f % i == 0
+  end
+end
+
+def largest_prime_factor(n)
+  prime_factors = []
+  n.downto(2) do |f|
+    if n % f == 0 && is_prime?(f)
+      prime_factors << f
+    end
+  end
+  return prime_factors.max
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+
+  def initialize(n)
+    @n = n
+    @largest_prime = nil
+  end
+
+  def is_prime?(f) 
+    Math.sqrt(f).to_i.downto(2).each do |i| 
+      return false if f % i == 0
+    end
+  end
+
+  def number
+    prime_factors = []
+    @n.downto(2) do |f|
+      if @n % f == 0 && is_prime?(f)
+        prime_factors << f
+      end
+    end
+    @largest_prime = prime_factors.max
+  end
+
+end
